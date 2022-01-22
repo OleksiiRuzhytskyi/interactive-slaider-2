1220f14d2e4f433463b7e4a90eab11584173be2a
Oleksii Ruzhytskyi <oleksiiruzhytskyi@gmail.com>
2022-01-22 14:55:42 +0200

    fisish task

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 let arrowRight = document.querySelector('.arrow-right');
 let arrowLeft = document.querySelector('.arrow-left');
+let slider = document.querySelector('.container__slider');
 const imgSlider = document.querySelectorAll('.img-slaider')
 const allFruitPep = document.querySelectorAll('.fruitDel')
 let countImg = 0
+
+let coordX1
+let coordY1
+
+slider.addEventListener('touchstart', function (e) {
+	let eTouches = e.touches[0]
+	coordX1 = eTouches.clientX
+	coordY1 = eTouches.clientY
+})
+
+slider.addEventListener('touchmove', function (e) {
+	if (!coordX1 || !coordY1) return
+	let eTouches = e.touches[0]
+	let coordX2 = eTouches.clientX
+	let coordY2 = eTouches.clientY
+
+	let diffX = coordX2 - coordX1
+	let diffY = coordY2 - coordY1
+
+	if (Math.abs(diffX) > Math.abs(diffY)) {
+		if (diffX > 0) {
+			changeElement()
+		} else {
+			changeElement()
+		}
+	}
+	coordX1 = null
+	coordX2 = null
+})
 
 arrowLeft.addEventListener('click', () => {
 	changeElement()
