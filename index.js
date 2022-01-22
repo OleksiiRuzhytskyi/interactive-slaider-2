a9e5a71a9e2ece84c10b46e88324f120bb075645
Oleksii Ruzhytskyi <oleksiiruzhytskyi@gmail.com>
2022-01-23 01:38:29 +0200

    change some tags

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+
 let arrowRight = document.querySelector('.arrow-right');
 let arrowLeft = document.querySelector('.arrow-left');
 let slider = document.querySelector('.container__slider');
 const imgSlider = document.querySelectorAll('.img-slaider')
-const allFruitPep = document.querySelectorAll('.fruitDel')
+const allFruitPep = document.querySelectorAll('.all-fruits-pep-container')
 let countImg = 0
 
 let coordX1
@@ -38,7 +39,7 @@ arrowLeft.addEventListener('click', () => {
 	changeElement()
 })
 
-arrowRight.addEventListener('click',function() {
+arrowRight.addEventListener('click', function () {
 	changeElement()
 })
 
@@ -46,31 +47,25 @@ function changeElement() {
 	for (let i = 0; i < imgSlider.length; i++) {
 		imgSlider[i].classList.add('opacity-0')
 		console.log('i: ', i);
-	}	
+	}
 	console.log('atr: ', imgSlider[countImg].getAttribute('data'))
 	console.log(typeof imgSlider[countImg].getAttribute('data'))
-	
+
 	if (imgSlider[countImg].getAttribute('data') === '2') {
 		for (let i = 0; i < allFruitPep.length; i++) {
 			allFruitPep[i].style.transition = 'all 1s';
 			allFruitPep[i].classList.remove('opacity-0')
 		}
-		document.querySelector('.pepper-container').classList.add('bg-peppermint')
-		document.querySelector('.pepper-container').classList.remove('bg-raspberry')
 		document.querySelector('.pep-container').classList.remove('opacity-0')
 		document.querySelector('.raspberry-container').classList.remove('opacity-1')
-		document.querySelector('.peach-container').classList.add('opacity-0')
 	} else if (imgSlider[countImg].getAttribute('data') === '1') {
-		document.querySelector('.peach-container').classList.add('opacity-1')
 		document.querySelector('.pep-container').style.transition = 'all 1s';
-		document.querySelector('.pep-container').classList.add('opacity-0')
 		for (let i = 0; i < allFruitPep.length; i++) {
 			allFruitPep[i].style.transition = 'all 1s';
 			allFruitPep[i].classList.add('opacity-0')
 		}
 		document.querySelector('.raspberry-container, .raspberry-1 ').classList.add('opacity-1')
 		document.querySelector('.raspberry-container').style.transition = 'all 1s';
-		document.querySelector('.pepper-container').classList.add('bg-raspberry')
 		document.querySelector('.peach-container').classList.add('opacity-0')
 
 	} else if (imgSlider[countImg].getAttribute('data') === '0') {
@@ -78,10 +73,9 @@ function changeElement() {
 			allFruitPep[i].style.transition = 'all 1s';
 			allFruitPep[i].classList.add('opacity-0')
 		}
-		document.querySelector('.raspberry-container').classList.remove('opacity-1')
-		document.querySelector('.pepper-container').classList.add('bg-peach')
+		document.querySelector('.pep-container').classList.add('opacity-0')
+		document.querySelector('.main-container').classList.add('bg-peach')
 		document.querySelector('.peach-container').classList.remove('opacity-0')
-		document.querySelector('.peach-container').classList.add('opacity-1')
 		document.querySelector('.peach-container').style.transition = 'all 1s';
 		console.log('400: ', document.querySelector('.peach-container'));
 	}
@@ -92,4 +86,3 @@ function changeElement() {
 		countImg++
 	}
 }
-
